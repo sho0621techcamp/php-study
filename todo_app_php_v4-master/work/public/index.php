f2bfd65ec7ccb2614fd8fe65f2f5381819558e43
sho0621techcamp <sho6217@gmail.com>
2021-04-18 11:40:50 +0900

    ファイルの切り分け

--- todo_app_php_v4-master/work/public/index.php
+++ todo_app_php_v4-master/work/public/index.php
@@ -1,73 +1,10 @@
 <?php
 
-	session_start();
-
-	define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
-	define('DB_USER', 'myappuser');
-	define('DB_PASS', 'myapppass');
-	//define('SITE_URL', 'http://localhost:8562');
-	// ↓に書き換えられる
-	define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+	require_once(__DIR__ . '/../app/config.php');
 
 	createToken();
 
-	try{
-		$pdo = new PDO(
-			DSN,
-			DB_USER,
-			DB_PASS,
-			[
-				//DB接続が失敗したらエラーを出す
-				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-
-				// Fetchモードのオプション　オブジェクト形式で結果を取得する
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-
-				PDO::ATTR_EMULATE_PREPARES => false
-			]
-		);
-	} catch (PDOException $e) {
-		echo $e->getMessage();
-		exit;
-	}
-
-	function h($str)
-	{
-		return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-	}
-
-	function createToken()
-	{
-		if (!isset($_SESSION['token'])) {
-			$_SESSION['token'] = bin2hex(random_bytes(32));
-		}
-	}
-
-	function validateToken()
-	{
-		if (empty($_SESSION['token']) || $_SESSION['token'] !== filter_input(INPUT_POST, 'token')) {
-			exit('Invalid post request');
-		}
-	}
-
-	function addTodo($pdo)
-	{
-		$title = trim(filter_input(INPUT_POST, 'title'));
-		if ($title === '') {
-			return;
-		}
-
-		$stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
-		$stmt->bindValue('title', $title, PDO::PARAM_STR);
-		$stmt->execute();
-	}
-
-	function getTodos($pdo)
-	{
-		$stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
-		$todos = $stmt->fetchALL();
-		return $todos;
-	}
+	$pdo = getPdoInstance();
 
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		//フォームが送信された時に埋め込んだ値とセッションのトークンが一致するか
