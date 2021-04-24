79c6e3e791cd1288c510606620865cffed836c10
sho0621techcamp <sho6217@gmail.com>
2021-04-24 12:11:54 +0900

    PDO切り出し（Databaseクラス作成）

---
+++ todo_app_php_v4-master/work/app/Database.php
@@ -0,0 +1,32 @@
+<?php
+	class Database
+	{
+		private static $instance;
+
+		public static function getInstance()
+		{
+			try{
+				if (!isset(self::$instance)) {
+					self::$instance = new PDO(
+						DSN,
+						DB_USER,
+						DB_PASS,
+						[
+							//DB接続が失敗したらエラーを出す
+							PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+
+							// Fetchモードのオプション　オブジェクト形式で結果を取得する
+							PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+
+							PDO::ATTR_EMULATE_PREPARES => false
+						]
+					);
+				}
+				return self::$instance;
+			} catch (PDOException $e) {
+				echo $e->getMessage();
+				exit;
+			}
+		}
+
+	}
