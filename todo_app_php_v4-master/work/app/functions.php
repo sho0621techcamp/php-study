d9d5c8b818abf7a03441535403facd5121c26c59
sho0621techcamp <sho6217@gmail.com>
2021-04-24 10:48:35 +0900

    削除機能

--- todo_app_php_v4-master/work/app/functions.php
+++ todo_app_php_v4-master/work/app/functions.php
@@ -1,78 +1,91 @@
 <?php
 
-  function h($str)
-  {
-	return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-  }
-
-  function createToken()
-  {
-	if (!isset($_SESSION['token'])) {
-	  $_SESSION['token'] = bin2hex(random_bytes(32));
-	}
-  }
-
-  function validateToken()
-  {
-	if (empty($_SESSION['token']) || $_SESSION['token'] !== filter_input(INPUT_POST, 'token')) {
-	  exit('Invalid post request');
-	}
-  }
-
-  function getPdoInstance()
-  {
-	try{
-		$pdo = new PDO(
-			DSN,
-			DB_USER,
-			DB_PASS,
-			[
-				//DB接続が失敗したらエラーを出す
-				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-
-				// Fetchモードのオプション　オブジェクト形式で結果を取得する
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-
-				PDO::ATTR_EMULATE_PREPARES => false
-			]
-		);
-
-		return $pdo;
-	} catch (PDOException $e) {
-		echo $e->getMessage();
-		exit;
-	}
-  }
-
-  function addTodo($pdo)
-  {
-	$title = trim(filter_input(INPUT_POST, 'title'));
-	if ($title === '') {
-	  return;
+	function h($str)
+	{
+		return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 	}
 
-	$stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
-	$stmt->bindValue('title', $title, PDO::PARAM_STR);
-	$stmt->execute();
-  }
-
-  function toggleTodo($pdo)
-  {
-	$id = filter_input(INPUT_POST, 'id');
-	
-	if (empty($id)) {
-		return;
+	function createToken()
+	{
+		if (!isset($_SESSION['token'])) {
+			$_SESSION['token'] = bin2hex(random_bytes(32));
+	}
 	}
 
-	$stmt = $pdo->prepare("UPDATE todos SET is_done = NOT is_done WHERE id = :id");
-	$stmt->bindValue('id', $id, PDO::PARAM_INT);
-	$stmt->execute();
+	function validateToken()
+	{
+		if (empty($_SESSION['token']) || $_SESSION['token'] !== filter_input(INPUT_POST, 'token')) {
+			exit('Invalid post request');
+		}
+	}
 
-  }
+	function getPdoInstance()
+	{
+		try{
+			$pdo = new PDO(
+				DSN,
+				DB_USER,
+				DB_PASS,
+				[
+					//DB接続が失敗したらエラーを出す
+					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 
-  function getTodos($pdo)
-  {
-	$stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
-	$todos = $stmt->fetchALL();
-	return $todos;
-  }
+					// Fetchモードのオプション　オブジェクト形式で結果を取得する
+					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+
+					PDO::ATTR_EMULATE_PREPARES => false
+				]
+			);
+
+			return $pdo;
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+			exit;
+		}
+	}
+
+	function addTodo($pdo)
+	{
+		$title = trim(filter_input(INPUT_POST, 'title'));
+		if ($title === '') {
+			return;
+		}
+
+		$stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
+		$stmt->bindValue('title', $title, PDO::PARAM_STR);
+		$stmt->execute();
+	}
+
+	function toggleTodo($pdo)
+	{
+		$id = filter_input(INPUT_POST, 'id');
+
+		if (empty($id)) {
+			return;
+		}
+
+		$stmt = $pdo->prepare("UPDATE todos SET is_done = NOT is_done WHERE id = :id");
+		$stmt->bindValue('id', $id, PDO::PARAM_INT);
+		$stmt->execute();
+
+	}
+
+	function getTodos($pdo)
+	{
+		$stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
+		$todos = $stmt->fetchALL();
+		return $todos;
+	}
+
+	function deleteTodo($pdo)
+	{
+		$id = filter_input(INPUT_POST, 'id');
+
+		if (empty($id)) {
+			return;
+		}
+
+		$stmt = $pdo->prepare("DELETE FROM todos WHERE id = :id");
+		$stmt->bindValue('id', $id, PDO::PARAM_INT);
+		$stmt->execute();
+	}	
